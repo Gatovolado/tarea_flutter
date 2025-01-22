17f345ec571c5081f25b81671ec2814b0c7962f3
Gatovolado <codigothecratos@gmail.com>
2025-01-22 12:48:57 +0100

    Lista de favoritos y siguiente personaje

--- lib/personaje.dart
+++ lib/personaje.dart
@@ -1,14 +1,16 @@
 class Character {
+  final String url;
   final String name;
   final String gender;
   final String culture;
   final String born;
 
-  const Character({required this.name, required this.gender, 
+
+  const Character({required this.url, required this.name, required this.gender, 
                     required this.culture, required this.born});
   
   factory Character.fromJson(Map<String, dynamic> json){
-    return Character(name: json['name'] as String, gender: json['gender'] as String, 
+    return Character(url: json['url'], name: json['name'] as String, gender: json['gender'] as String, 
                       culture: json['culture'] as String, born: json['born'] as String);
   }
 }
